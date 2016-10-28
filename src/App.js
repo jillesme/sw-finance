a237f6fe5687ea67605c791121804ebbfeb5bd17
Jilles Soeters <jsoeters@xebia.com>
2016-10-28 10:35:15 +0200

    add sw and remove logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      online: navigator.onLine
+    };
+
+    const listener = () => this.setState({ online: navigator.onLine });
+    window.addEventListener('online', listener);
+    window.addEventListener('offline', listener);
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to SW-Finance</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          You're curently <strong>{this.state.online ? 'online' : 'offline' }</strong>
         </p>
       </div>
     );
